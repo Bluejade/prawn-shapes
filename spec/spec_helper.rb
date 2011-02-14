0b618b3739bc8f5e210cfbf09e412bf594fe6017
Daniel Nelson <daniel@optiplex760.(none)>
2011-02-14 13:58:51 -0600

    Bundler.setup in spec helper so can run individual specs with ruby spec/path_to_file.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 # encoding: utf-8
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "lib")
 
 require "rubygems"
+require "bundler"
+Bundler.setup
 
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "lib")
 require "prawn"
 
 require "test/spec"
